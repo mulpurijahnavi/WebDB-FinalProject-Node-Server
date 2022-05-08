dea2aea120309683d1d390b1ff96416720d56f62
Jahnavi Mulpuri <jmulpuri@gmail.com>
2022-05-08 18:50:51 -0400

    Added CORS Policy

--- index.js
+++ index.js
@@ -24,6 +24,13 @@ const server = http.createServer((req, res) => {
             break;
     }
 
+    const headers = {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "OPTIONS, POST, GET",
+        "Access-Control-Max-Age": 2592000, // 30 days
+        "Content-Type": contentType 
+      };
+
     // readthe file
     fs.readFile(filePath, (err, content) => {
         // err.code
@@ -42,7 +49,7 @@ const server = http.createServer((req, res) => {
             }
         } else {
             //sucess
-            res.writeHead(200, { 'Content-Type': contentType })
+            res.writeHead(200, headers)
             res.end(content, 'utf-8')
         }
     });
